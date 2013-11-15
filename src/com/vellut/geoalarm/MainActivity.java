affc75fc08ae76d89b014a9f2daeea8b8c39ed4e
guilhem <guilhem.vellut@gmail.com>
2013-11-15 21:42:11 +0900

    Added GeoFence activity Changed layout of main with test controls

--- src/com/vellut/geoalarm/MainActivity.java
+++ src/com/vellut/geoalarm/MainActivity.java
@@ -2,53 +2,21 @@ package com.vellut.geoalarm;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+import android.view.Menu;
 
 public class MainActivity extends FragmentActivity {
 
-		/* 
-	 	* 	Note that this may be null if the Google Play services APK is not available.
-	    */
-	    private GoogleMap mMap;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
 
-	    @Override
-	    protected void onCreate(Bundle savedInstanceState) {
-	        super.onCreate(savedInstanceState);
-	        setContentView(R.layout.activity_main);
-	        setUpMapIfNeeded();
-	    }
-
-	    @Override
-	    protected void onResume() {
-	        super.onResume();
-	        setUpMapIfNeeded();
-	    }
-
-	    private void setUpMapIfNeeded() {
-	        // Do a null check to confirm that we have not already instantiated the map.
-	        if (mMap == null) {
-	            // Try to obtain the map from the SupportMapFragment.
-	            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
-	                    .getMap();
-	            // Check if we were successful in obtaining the map.
-	            if (mMap != null) {
-	                setUpMap();
-	            }
-	        }
-	    }
-
-	    /**
-	     * This is where we can add markers or lines, add listeners or move the camera. In this case, we
-	     * just add a marker near Africa.
-	     * <p>
-	     * This should only be called once and when we are sure that {@link #mMap} is not null.
-	     */
-	    private void setUpMap() {
-	        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 45)).title("Marker"));
-	    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 
 }
